4f1d790ca971f80bc943ba0000ba7dd8d71d38fa
SpaceRock <dumbinternetalias@gmail.com>
2022-11-07 18:14:48 -0500

    Fix priority filter for loot

--- src/lootrule.js
+++ src/lootrule.js
@@ -8,7 +8,7 @@ export function resolve_loot_rollers(drops, loot_rule, role_map) {
     if (loot_rule === LootRule.PriorityFFA) {
         for (const id in drops) {
             const priority_role = drops[id].rollers.reduce((r, x) => Math.min(r, x.role), RaidRole.Unknown);
-            drops[id].rollers = drops[id].rollers.filter((x) => x.user_id);
+            drops[id].rollers = drops[id].rollers.filter((x) => x.role == priority_role);
         }
     } else {
         throw Error('Unknown loot rule');
