f088f1a124a2ac07dcafac60c4335811e5661346
SpaceRock <dumbinternetalias@gmail.com>
2022-11-07 18:14:48 -0500

    Implement loot rule function

---
+++ src/lootrule.js
@@ -0,0 +1,17 @@
+import { RaidRole } from "./enum.js";
+
+export const LootRule = Object.freeze({
+    PriorityFFA:    0,
+});
+
+export function resolve_loot_rollers(drops, loot_rule, role_map) {
+    if (loot_rule === LootRule.PriorityFFA) {
+        for (const id in drops) {
+            const priority_role = drops[id].rollers.reduce((r, x) => Math.min(r, x.role), RaidRole.Unknown);
+            drops[id].rollers = drops[id].rollers.filter((x) => x.user_id);
+        }
+    } else {
+        throw Error('Unknown loot rule');
+    }
+    return drops;
+}
